15186446134be22f6daae2219a519e3801c90200
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2024-09-17 19:07:43 +1000

    Remove template code Fixes #5

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -5,29 +5,56 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { GitHubSignInButton } from "@/components/github-sign-in-button";
+import { SmtpMessage } from "../smtp-message";
 
 export default function SignUp({ searchParams }: { searchParams: Message }) {
-	return (
-		<form className="flex-1 flex flex-col min-w-64">
-			{/* ... existing code ... */}
-			<div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-				{/* ... existing form fields ... */}
-				<SubmitButton pendingText="Signing Up..." formAction={signUpAction}>
-					Sign up
-				</SubmitButton>
-				<div className="relative my-4">
-					<div className="absolute inset-0 flex items-center">
-						<span className="w-full border-t" />
-					</div>
-					<div className="relative flex justify-center text-xs uppercase">
-						<span className="bg-background px-2 text-muted-foreground">
-							Or continue with
-						</span>
-					</div>
-				</div>
-				<GitHubSignInButton />
+	if ("message" in searchParams) {
+		return (
+			<div className="w-full flex-1 flex items-center h-screen sm:max-w-md justify-center gap-2 p-4">
 				<FormMessage message={searchParams} />
 			</div>
-		</form>
+		);
+	}
+
+	return (
+		<>
+			<form className="flex flex-col min-w-64 max-w-64 mx-auto">
+				<h1 className="text-2xl font-medium">Sign up</h1>
+				<p className="text-sm text text-foreground">
+					Already have an account?{" "}
+					<Link className="text-primary font-medium underline" href="/sign-in">
+						Sign in
+					</Link>
+				</p>
+				<div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+					<Label htmlFor="email">Email</Label>
+					<Input name="email" placeholder="you@example.com" required />
+					<Label htmlFor="password">Password</Label>
+					<Input
+						type="password"
+						name="password"
+						placeholder="Your password"
+						minLength={6}
+						required
+					/>
+					<SubmitButton formAction={signUpAction} pendingText="Signing up...">
+						Sign up
+					</SubmitButton>
+					<div className="relative my-4">
+						<div className="absolute inset-0 flex items-center">
+							<span className="w-full border-t" />
+						</div>
+						<div className="relative flex justify-center text-xs uppercase">
+							<span className="bg-background px-2 text-muted-foreground">
+								Or continue with
+							</span>
+						</div>
+					</div>
+					<GitHubSignInButton />
+					<FormMessage message={searchParams} />
+				</div>
+			</form>
+			<SmtpMessage />
+		</>
 	);
 }
