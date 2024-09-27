999d95bc548f86ddb5cf3408b14a0e83a525ecc4
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2024-09-27 14:30:51 +1000

    Simplified it

--- components/developer-dashboard.tsx
+++ components/developer-dashboard.tsx
@@ -1,3 +1,4 @@
+"use server";
 import Link from "next/link";
 import { createClient } from "@/utils/supabase/server";
 import { Button } from "@/components/ui/button";
--- lib/appUrl.ts
+++ lib/appUrl.ts
@@ -1,26 +1,9 @@
-export function appUrl(slug: string, headers?: Headers) {
+export function appUrl(slug: string) {
 	// Updated function signature
 	const baseUrl =
 		typeof window !== "undefined"
 			? window.location.origin
-			: headers
-				? `https://${headers.get("host")}`
-				: `https://${process.env.NEXT_PUBLIC_VERCEL_URL || "localhost:3000"}`;
-
-	console.log("appUrl", baseUrl, slug);
-	console.log(
-		"window.location.origin",
-		typeof window !== "undefined" ? window.location.origin : undefined
-	);
-	console.log(
-		"headers",
-		typeof headers !== "undefined" ? headers.get("host") : undefined
-	);
-	console.log(
-		"NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL",
-		process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
-	);
-	console.log("NEXT_PUBLIC_VERCEL_URL", process.env.NEXT_PUBLIC_VERCEL_URL);
+			: `https://${process.env.NEXT_PUBLIC_VERCEL_URL || "localhost:3000"}`;
 
 	return `${baseUrl}/a/${slug}`;
 }
