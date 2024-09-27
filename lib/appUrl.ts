b255ef8338fa517e27f79a28e2bb75b031697e4a
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2024-09-27 14:10:15 +1000

    Added more logging

--- lib/appUrl.ts
+++ lib/appUrl.ts
@@ -5,5 +5,15 @@ export function appUrl(slug: string) {
 			: `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL || "localhost:3000"}`;
 
 	console.log("appUrl", baseUrl, slug);
+	console.log(
+		"window.location.origin",
+		typeof window !== "undefined" ? window.location.origin : undefined
+	);
+	console.log(
+		"NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL",
+		process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+	);
+	console.log("NEXT_PUBLIC_VERCEL_URL", process.env.NEXT_PUBLIC_VERCEL_URL);
+
 	return `${baseUrl}/a/${slug}`;
 }
