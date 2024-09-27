1638a56f2c5d1c9809f19f165646891a0cb47792
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2024-09-27 14:20:08 +1000

    Trying one more thing

--- lib/appUrl.ts
+++ lib/appUrl.ts
@@ -1,13 +1,20 @@
-export function appUrl(slug: string) {
+export function appUrl(slug: string, headers?: Headers) {
+	// Updated function signature
 	const baseUrl =
 		typeof window !== "undefined"
 			? window.location.origin
-			: `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL || "localhost:3000"}`;
+			: headers
+				? `https://${headers.get("host")}`
+				: `https://${process.env.NEXT_PUBLIC_VERCEL_URL || "localhost:3000"}`;
 
 	console.log("appUrl", baseUrl, slug);
 	console.log(
 		"window.location.origin",
 		typeof window !== "undefined" ? window.location.origin : undefined
+	);
+	console.log(
+		"headers",
+		typeof headers !== "undefined" ? headers.get("host") : undefined
 	);
 	console.log(
 		"NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL",
