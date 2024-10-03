087a6eec9f52beffd2975ce042bfbcf745844de6
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2024-10-03 13:24:06 +1000

    Updated domain

--- app/api/create-setup-intent/route.ts
+++ app/api/create-setup-intent/route.ts
@@ -1,3 +1,4 @@
+import { appDomain, appUrl } from "@/lib/appUrl";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
@@ -6,12 +7,13 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 });
 
 export async function POST() {
+	appUrl;
 	try {
 		const session = await stripe.checkout.sessions.create({
 			payment_method_types: ["card"],
 			mode: "setup",
-			success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/protected/save-card-success?session_id={CHECKOUT_SESSION_ID}`,
-			cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/protected/save-card`,
+			success_url: `${appDomain()}/protected/save-card-success?session_id={CHECKOUT_SESSION_ID}`,
+			cancel_url: `${appDomain()}/protected/save-card`,
 		});
 
 		return NextResponse.json({ sessionId: session.id });
--- lib/appUrl.ts
+++ lib/appUrl.ts
@@ -1,9 +1,12 @@
+export function appDomain() {
+	// Updated function signature
+	return typeof window !== "undefined"
+		? window.location.origin
+		: `${process.env.VERCEL_ENV === "production" ? process.env.VERCEL_PROJECT_PRODUCTION_URL : process.env.NEXT_PUBLIC_VERCEL_URL || "localhost:3000"}`;
+}
+
 export function appUrl(slug: string) {
 	// Updated function signature
-	const baseUrl =
-		typeof window !== "undefined"
-			? window.location.origin
-			: `https://${process.env.NODE_ENV === "production" ? process.env.VERCEL_PROJECT_PRODUCTION_URL : process.env.NEXT_PUBLIC_VERCEL_URL || "localhost:3000"}`;
 
-	return `${baseUrl}/a/${slug}`;
+	return `https://${appDomain()}/a/${slug}`;
 }
