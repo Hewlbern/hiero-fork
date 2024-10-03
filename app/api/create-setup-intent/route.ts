41c9b0e3c536700140b43146a7db268f25d64984
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2024-10-03 12:38:25 +1000

    Add Stripe Fixes #35

---
+++ app/api/create-setup-intent/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+	apiVersion: "2024-09-30.acacia",
+});
+
+export async function POST() {
+	try {
+		const session = await stripe.checkout.sessions.create({
+			payment_method_types: ["card"],
+			mode: "setup",
+			success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/protected/save-card-success?session_id={CHECKOUT_SESSION_ID}`,
+			cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/protected/save-card`,
+		});
+
+		return NextResponse.json({ sessionId: session.id });
+	} catch (error) {
+		console.error("Error creating Checkout Session:", error);
+		return NextResponse.json(
+			{ error: "Unable to create Checkout Session" },
+			{ status: 500 }
+		);
+	}
+}
