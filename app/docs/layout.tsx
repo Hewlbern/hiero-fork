dda975f31cba61450ac169d13d56d685ad512338
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2024-10-17 15:09:53 +1100

    Added missing pages Fixes #41

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -6,6 +6,73 @@ import Link from "next/link";
 // Import Shadcn UI components
 import { ScrollArea } from "@/components/ui/scroll-area";
 
+const sidebarItems = [
+	{
+		title: "Getting Started",
+
+		href: "/docs/getting-started",
+	},
+
+	{
+		title: "User Experience",
+		href: "/docs/user-experience",
+	},
+	{
+		title: "Launching Pay with Hiero",
+		href: "/docs/launching-pay-with-hiero",
+	},
+	{
+		title: "Webhooks",
+		href: "/docs/webhooks",
+	},
+	{
+		title: "API Endpoints",
+		type: "heading",
+		href: "/docs/api-endpoints",
+		items: [
+			{
+				title: "Deduct Tokens",
+				href: "/docs/api-endpoints/deduct",
+			},
+			{
+				title: "Get Token Balance",
+				href: "/docs/api-endpoints/balance",
+			},
+		],
+	},
+];
+
+const getSidebarItems = (items: typeof sidebarItems) => {
+	return items.map((item) => {
+		if (item.type === "heading") {
+			return (
+				<li key={item.href}>
+					<p className="block px-4 py-2 ">{item.title}</p>
+					<ul className="space-y-2">
+						{item.items.map((subItem) => (
+							<li key={subItem.href}>
+								<Link
+									href={subItem.href}
+									className="block ml-4 px-4 py-2 hover:text-blue-400"
+								>
+									{subItem.title}
+								</Link>
+							</li>
+						))}
+					</ul>
+				</li>
+			);
+		}
+		return (
+			<li key={item.href}>
+				<Link href={item.href} className="block px-4 py-2 hover:text-blue-400">
+					{item.title}
+				</Link>
+			</li>
+		);
+	});
+};
+
 export default function DocsLayout({ children }: { children: ReactNode }) {
 	return (
 		<div className="flex">
@@ -13,33 +80,7 @@ export default function DocsLayout({ children }: { children: ReactNode }) {
 				<ScrollArea className="h-full">
 					<nav className="p-5">
 						<h2 className="text-2xl font-semibold mb-4">Documentation</h2>
-						<ul className="space-y-2">
-							<li>
-								<Link
-									href="/docs/getting-started"
-									className="block px-4 py-2 hover:text-blue-400"
-								>
-									Getting Started
-								</Link>
-							</li>
-							<li>
-								<Link
-									href="/docs/api-endpoints/deduct"
-									className="block px-4 py-2 hover:text-blue-400"
-								>
-									Deduct Tokens
-								</Link>
-							</li>
-							<li>
-								<Link
-									href="/docs/api-endpoints/balance"
-									className="block px-4 py-2 hover:text-blue-400"
-								>
-									Get Token Balance
-								</Link>
-							</li>
-							{/* ... other links ... */}
-						</ul>
+						<ul className="space-y-2">{getSidebarItems(sidebarItems)}</ul>
 					</nav>
 				</ScrollArea>
 			</aside>
